13de72a06a13475f11f6889af564433ba63bd76f
Mateus-Lucas <mateus.lucas.ferreira.07@gmail.com>
2024-05-03 21:46:07 -0300

    Chaves estrangeiras + seed

--- app/models/produto.ts
+++ app/models/produto.ts
@@ -5,6 +5,19 @@ export default class Produto extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
+  @column()
+  declare nome: string
+  
+
+  @column()
+  declare preco: number
+
+  @column()
+  declare tamanho: string
+
+  @column()
+  declare tipoId: number
+
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
